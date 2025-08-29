ffc400cf3e2a608fbcabe82e938e8d21f26023b0
ll9pm3 <ab.oodll9pm@gmail.com>
2025-08-29 22:23:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def main():
     """Creates an aiohttp session and fetches multiple URLs concurrently."""
     urls = list()
     apis =[
-        "XTpI4QKMvA1e6rg9itYnflyxZmS9YEyx","GUs6ndJYWAdO5I0z4lBMb09uLeDrdwyT"]
+        "R7iUjaH1LcQpdEvw8MKp95KQuxtnkZkK","Hyv0BjDvQxKYTqb8eCmDJ70rcFQ3IcGG"]
     ad="https://viidedss.com/dc/?blockID=386520"
     for api in apis:
         for _ in range(randint (3,5)):
