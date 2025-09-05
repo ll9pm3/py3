7c47ffc62957b9428b7bf2471d19efa66117ba24
ll9pm3 <ab.oodll9pm@gmail.com>
2025-09-05 17:38:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ async def main():
     """Creates an aiohttp session and fetches multiple URLs concurrently."""
     urls = list()
     apis =[
-        "8DnmiFRLRuTfqBtHJiltzV5NVSo38HLr","stgWEOhEhmtHoX787F3jYzWWADYRWTLP"]
-    ad="https://viidedss.com/dc/?blockID=388143"
+        "dtMrLmEeJQqDwZBAdbLVuBP2wOTdpdLL","n12nX4td9TtFGgZBSCAZsNKk3BrzddVR"]
+    ad="https://viidedss.com/dc/?blockID=388601"
     for api in apis:
         for _ in range(randint (3,5)):
             url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
