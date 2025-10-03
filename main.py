7d7f7be05828075dc2464867d4dffb4ae4b795a8
ll9pm3 <ab.oodll9pm@gmail.com>
2025-10-03 09:33:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def main():
     urls = list()
     apis =[
         "zMWEPOwx5msyEuXmjDNeGB2J2QOKDLvv","VeN3Lku77tDT9r9pM8WgdQy7FSJu17wc"]
-    ad="https://viijujyl.com/dc/?blockID=391319"
+    ad="https://viikqoye.com/dc/?blockID=392416"
     for api in apis:
         for _ in range(randint (3,5)):
             url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
